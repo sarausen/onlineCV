f4d5d135e23216e04bd470a7def39dcbe8540831
suusara <smataeva09@gmail.com>
2023-05-09 17:38:30 +0600

    add contacts page

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,7 +1,41 @@
+import "./style.css"
+
 export const ContactsPage = () => {
   return (
-    <div  className="container" >
-      ContactsPage
+    <div  className="ss" >
+      <div className="content_block">
+      <h2>Contacts</h2>
+      <p>You can sent me a letter right from here</p>
+      <div className="aaaa">
+      <div className="contacts_email"> 
+      <input type="text" placeholder="name" />
+      <input type="email" placeholder="email" />
+      <input type="file"  placeholder="file"/>
+      <textarea name="" id="" cols="30" rows="10">Your message here...</textarea>
+      <a href="mailto:smataeva09@gmail.com">
+       <button>Send message</button></a>
+      </div>
+      <div>
+      <a href="tg://resolve?domain=M S">
+    <button>Chat with Telegram</button></a>
+    <a href="https://www.linkedin.com/in/S_Mataeva/">
+     <button>Go to LinkedIn</button></a>
+
+    <a href="https://github.com/suusara">
+    <button>Go to GitHub</button></a>
+    <div className="contacts_contact">
+    <p>Kyrgyz Republic, Bishkek</p>
+    <p>smataeva09@gmail.com</p>
+    <p>+996 555 111 850</p>
+    </div>
+    
+
+      </div>
+      </div>
+   
+     
+      </div>
+      
     </div>
   )
 }
