78df0319b9964314ab7d939467a30bd6f2488b1a
suusara <smataeva09@gmail.com>
2023-05-09 15:35:48 +0600

    add about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,11 +2,13 @@ import "./style.css"
 
 export const AboutPage = () => {
   return (
-    <div className='container'>
-      
-
-      
-    
+    <div className='ss'>
+      <div className="content_block">
+        <h2>About me</h2>
+        <p className="ff">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+        <img className="about_img" src="https://abrakadabra.fun/uploads/posts/2022-01/1641405815_1-abrakadabra-fun-p-devushka-za-kompom-1.jpg" alt="" />
+        <p className="ff">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id. Praesent lorem orci, mattis non efficitur id, ultricies vel nibh. Sed volutpat lacus vitae gravida viverra. Fusce vel tempor elit. Proin tempus, magna id scelerisque vestibulum, nulla ex pharetra sapien, tempor posuere massa neque nec felis. Aliquam sem ipsum, vehicula ac tortor vel, egestas ullamcorper dui. Curabitur at risus sodales, tristique est id, euismod justo. Mauris nec leo non libero sodales lobortis. Quisque a neque pretium, dictum tellus vitae, euismod neque. Nulla facilisi. Phasellus ultricies dignissim nibh ut cursus. Nam et quam sit amet turpis finibus maximus tempor eget augue. Aenean at ultricies lorem. Sed egestas ligula tortor, sit amet mattis ex feugiat non. Duis purus diam, dictum et ante at, commodo iaculis urna. Aenean lacinia, nisl id vehicula condimentum, enim massa.</p>
+      </div>
     </div>
   )
 }
--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,7 +1,12 @@
 export const PortfolioPage = () => {
   return (
-    <div className="container">
-      PortfolioPage
+    <div className="ss">
+       <div className="content_block">
+        <h2>My Portfolio</h2>
+        <div>
+          
+        </div>
+       </div>
       </div>
   )
 }
